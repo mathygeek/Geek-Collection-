990de46f36077a3c73680b49425509b1e9ecbeed
mathygeek <jebamole74@gmail.com>
2025-12-29 13:22:51 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Projeto em construção 🚧</h1>
+      <p>Base do site criada com sucesso.</p>
+    </main>
+  )
+}
